8eaa40b8c8b5b415f06db73018c2da351d31b13f
testiosis <dosmatovislam62@gmail.com>
2021-12-27 21:17:45 +0600

    test

--- lesson1/main.swift
+++ lesson1/main.swift
@@ -5,7 +5,34 @@
 //  Created by islam on 24/12/21.
 //
 
-import Foundation
+//print("start")
 
-print("Hello, World!")
+//for i in 1...10{
+//    print("\(i) shag")
+    
+//}
 
+
+//var array2: [String] = []
+//array2.append("nursultan")
+
+//array2.append("bakyt")
+//array2.append("islam")
+
+//print(array2)
+//array2.reverse()
+//print(array2)
+
+
+
+//for i in array2 {
+//    if i == "islam"
+        
+//}
+
+//var lessons = 0
+//for i in 1...4{
+//  lessons += 2
+    
+//}
+//print("v mesyase \(lessons) urokov")
